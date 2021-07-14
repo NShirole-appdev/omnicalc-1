828fecfa5a1d00bb8962d4d21f006af561917c38
Nina Shirole <ninashirole@uchicago.edu>
2021-07-14 03:31:16 +0000

    sqrt and random work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,27 @@ class ApplicationController < ActionController::Base
 
     render({ :template => "calculation_templates/square_results.html.erb"})
  end
+
+def blank_random_form
+    render({ :template => "calculation_templates/random_form.html.erb"})
+end
+
+
+ def calculate_random
+
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @output = rand((@lower..@upper))
+    render({ :template => "calculation_templates/rand_results.html.erb"})
+ end
+def blank_sqrt_form
+    render({ :template => "calculation_templates/sqrt_form.html.erb"})
+end
+def calculate_sqrt
+
+    @sqrtnum = params.fetch("sqrt_input").to_f
+    @sqrt_of_num = Math.sqrt(@sqrtnum)
+    render({ :template => "calculation_templates/sqrt_results.html.erb"})
+end
+
 end
