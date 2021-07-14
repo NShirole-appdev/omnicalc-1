4278515387b05b5619abfe17b3969e98731f5906
Nina Shirole <ninashirole@uchicago.edu>
2021-07-14 06:02:35 +0000

    all fo the work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,4 +34,25 @@ def calculate_sqrt
     render({ :template => "calculation_templates/sqrt_results.html.erb"})
 end
 
+def blank_payment_form
+    render({ :template => "calculation_templates/payment_form.html.erb"})
+end
+
+def calculate_payment
+    @apr = params.fetch("user_apr").to_f
+    @percentageapr = @apr / 100
+    @rate_per_period = @percentageapr / 12
+    @rpp_plus_1 = @rate_per_period + 1
+    @years = params.fetch("user_years").to_f
+    @n = @years * 12
+    @negative_n = @n * -1
+    @principal = params.fetch("user_principal").to_f
+    @numerator = @rate_per_period * @principal
+    @denom_paren = @rpp_plus_1 ** @negative_n
+    @denominator = 1 - @denom_paren
+    @payment = @numerator / @denominator
+    @paymentfinal = @payment.round(2)
+    render({ :template => "calculation_templates/payment_results.html.erb"})
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     get("/random/results", { :controller => "application", :action => "calculate_random"})
     get("/square_root/new", { :controller => "application", :action => "blank_sqrt_form"})
     get("/square_root/results", { :controller => "application", :action => "calculate_sqrt"})
-
+    get("/payment/new", { :controller => "application", :action => "blank_payment_form"})
+    get("/payment/results", { :controller => "application", :action => "calculate_payment"})
 end
